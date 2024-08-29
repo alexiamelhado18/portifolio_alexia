9beba26e2cb4bbd51243be854e02fe7944e243c5
unknown <Alexia@DESKTOP-AP18MAI.info.local>
2024-08-29 13:38:17 -0300

    feat: adicionado componente e aplicado logica para sua exibição

--- frontend/src/app/components/Banner/index.tsx
+++ frontend/src/app/components/Banner/index.tsx
@@ -8,10 +8,21 @@ import Link from "next/link";
 //hooks
 import { useEffect, useLayoutEffect, useState } from "react";
 
+//componentes
+import { MenuResponsivoModal } from "../MenuResponsivoModal";
 
 export function Banner() {
 
     const [responsivo, setResponsivo] = useState(false);
+    const [abrirModalMenuResponsivo, setAbrirModalMenuResponsivo] = useState<boolean>(false);
+
+    function abrirMenuResponsivo() {
+        setAbrirModalMenuResponsivo(true);
+    }
+
+    function fecharMenuResponsivo() {
+        setAbrirModalMenuResponsivo(false);
+    }
 
     useLayoutEffect(() => {
         window.addEventListener("resize", () => {
@@ -25,39 +36,52 @@ export function Banner() {
     }, []);
 
     return (
-        <div className="div__banner" style={responsivo ? { background: "linear-gradient(260deg, rgba(255, 225, 214, 1) 0%, rgba(236, 227, 255, 1) 100%)" } : { backgroundImage: `url("/assets/images/svg/Banner.svg")`, backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center" }}>
-            <header>
-                <img src="/assets/images/svg/Logo.svg" alt="Logo que representa um tag html, formada pela letra A, / e V, deitados na horizontal." />
+        <>
+            <div className="div__banner" style={responsivo ? { background: "linear-gradient(260deg, rgba(255, 225, 214, 1) 0%, rgba(236, 227, 255, 1) 100%)" } : { backgroundImage: `url("/assets/images/svg/Banner.svg")`, backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center" }}>
+                <header>
+                    <img src="/assets/images/svg/Logo.svg" alt="Logo que representa um tag html, formada pela letra A, / e V, deitados na horizontal." />
 
-                <img src="/assets/images/png/abrir.png" alt="ícone que representa um menu" id="img__abrir" />
-            </header>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <div className="div__seta"></div>
-                            <Link href={"#"}>inicial</Link>
-                        </li>
-                        <li>
-                            <Link href={"#"}>sobre mim</Link>
-                        </li>
-                        <li>
-                            <Link href={"#"}>experiência</Link>
-                        </li>
-                        <li>
-                            <Link href={"#"}>projetos</Link>
-                        </li>
-                        <li>
-                            <Link href={"#"}>contato</Link>
-                        </li>
-                    </ul>
-                </nav>
+                    <button type="button" onClick={abrirMenuResponsivo}>
+                        <img src="/assets/images/png/abrir.png" alt="ícone que representa um menu" id="img__abrir" />
+                    </button>
+                </header>
                 <div>
-                    <p>
-                        Seja bem-vindo(a) ao meu <span>portfólio</span>
-                    </p>
+                    <nav>
+                        <ul>
+                            <li>
+                                <div className="div__seta"></div>
+                                <Link href={"#"}>inicial</Link>
+                            </li>
+                            <li>
+                                <Link href={"#"}>sobre mim</Link>
+                            </li>
+                            <li>
+                                <Link href={"#"}>experiência</Link>
+                            </li>
+                            <li>
+                                <Link href={"#"}>projetos</Link>
+                            </li>
+                            <li>
+                                <Link href={"#"}>contato</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div>
+                        <p>
+                            Seja bem-vindo(a) ao meu <span>portfólio</span>
+                        </p>
+                    </div>
                 </div>
-            </div>
-        </div >
+            </div >
+
+            {
+                abrirModalMenuResponsivo && (
+                    <MenuResponsivoModal
+                        fecharMenuResponsivo={fecharMenuResponsivo}
+                    />
+                )
+            }
+        </>
+
     );
 }
