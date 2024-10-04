5c79887c9eafad6cc05542575ce14dba208a41e1
unknown <Alexia@DESKTOP-AP18MAI.info.local>
2024-10-04 14:11:26 -0300

    feat: inclusão de h1 para titulo principal do site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body>
+        <h1 style={{ position: "absolute", opacity: 0 }}>Página inicial - portfólio de Aléxia Vitória</h1>
         <Banner />
         <main>
           {children}
